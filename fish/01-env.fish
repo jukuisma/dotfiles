c1f4387a82cc4784c0ac60a5e9881b002de6eb79
Juho Kuisma <kuisma.juho+github@gmail.com>
2025-03-17 18:58:49 +0200

    Disable vi keybindings in fish
    Those are unmaintained and seemed to systematically cause random
    hanging.
    
    https://github.com/fish-shell/fish-shell/issues/11109#issuecomment-2627422095

--- fish/01-env.fish
+++ fish/01-env.fish
@@ -14,10 +14,6 @@ if status is-interactive
     set -gx VAGRANT_DEFAULT_PROVIDER libvirt
     set -gx XM5_UUID
 
-    # Do not show vi mode on the prompt
-    function fish_mode_prompt; end
-    set -gx fish_key_bindings fish_vi_key_bindings
-
     # Do not edit prompt by venv/bin/activate.fish.
     # This is handled by 02-prompt.fish instead.
     set -gx VIRTUAL_ENV_DISABLE_PROMPT 1
