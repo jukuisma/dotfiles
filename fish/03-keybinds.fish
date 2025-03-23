436cdaa9c97bf0af19f62aaf775c7d78b19cfab1
Juho Kuisma <kuisma.juho+github@gmail.com>
2025-03-23 19:45:07 +0200

    Fix fish keybinds after dropping vi bindings

--- fish/03-keybinds.fish
+++ fish/03-keybinds.fish
@@ -2,15 +2,14 @@ if status is-interactive
 
     # Bind C-f to run tmux-sessionizer in both normal and insert modes.
     # Clear terminal and repaint after exiting tmux to avoid buggy behavior.
-    bind \cf --mode default "tmux-sessionizer; clear; commandline -f repaint"
-    bind \cf --mode insert "tmux-sessionizer; clear; commandline -f repaint"
+    bind \cf "tmux-sessionizer; clear; commandline -f repaint"
 
     # Bind C-y and C-space to accept autosuggestions.
-    bind \cy --mode insert accept-autosuggestion
-    bind -k --mode insert nul accept-autosuggestion
+    bind \cy accept-autosuggestion
+    bind -k nul accept-autosuggestion
 
     # Emacs
-    bind --mode insert \ce end-of-line
-    bind --mode insert \ca beginning-of-line
+    bind \ce end-of-line
+    bind \ca beginning-of-line
 
 end
